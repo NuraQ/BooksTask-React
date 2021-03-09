c1f8f3b290c207bf68ffbf06ee9fc6e2d0130c20
NuraQ <nura.qasrawi@gmail.com>
2021-03-09 16:10:30 +0200

    testing heroku

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,98 +1,122 @@
 import {
-    AppBar,
-    Toolbar,
-    Typography,
-    Button,
-    InputBase,
-  } from "@material-ui/core";
-  import React from "react";
-  import { useStyles } from "./NavBar.styles";
-  import { NavLink as RouterLink } from "react-router-dom";
-  import SearchIcon from "@material-ui/icons/Search";
-  import HomeIcon from "@material-ui/icons/Home";
-  import { useState } from "react";
-  import {IconButton} from "@material-ui/core";
-  import { Menu, MenuItem } from "@material-ui/core";
-  
-  const headersData = [
-    {
-      label: "Home",
-      href: "/Home",
-    },
-    {
-      label: "Purchased Books",
-      href: "/PurchasedBooks",
-    },
-    {
-      label: "Log Out",
-      href: "/logout",
-    },
-  ];
-  
-  const Header = () => {
-    const {
-      header,
-      logoStyle,
-      menuButton,
-      search,
-      searchIcon,
-      inputRoot,
-      inputInput,
-      menuIcon,
-    } = useStyles();
-  
-    const [state, setState] = useState({
-      openMenu: false,
-      anchorEl: null,
-    });
-    const { openMenu, anchorEl } = state;
-    const logo = (
-      <Typography variant="h6" component="h1" className={logoStyle}>
-        Book Store
-      </Typography>
-    );
-  
-    const setMobileView = (event) => {
-        let target = event.currentTarget
-      setState((prevState) => ({ ...prevState, openMenu: true }));
-      setState((prevState) => ({ ...prevState, anchorEl: target }));
-    };
-  
-    const handleClose = () => {
-      setState((prevState) => ({ ...prevState, openMenu: false }));
-      setState((prevState) => ({ ...prevState, anchorEl: null }));
-    };
-    const MenuBar = () => {
-      return (
-        <Toolbar>
-          <IconButton onClick={setMobileView}>
-            <HomeIcon className={menuIcon} />
-          </IconButton>
-          {logo}
-          {openMenu ? <MobileDisplay /> : MenuDesktop()}
-          <div className={search}>
-            <div className={searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              className={`${inputRoot} ${inputInput}`}
-              inputProps={{ "aria-label": "search" }}
-            />
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  InputBase,
+} from "@material-ui/core";
+import React from "react";
+import { useStyles } from "./NavBar.styles";
+import { NavLink as RouterLink } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import { useState } from "react";
+import { IconButton } from "@material-ui/core";
+import { Menu, MenuItem } from "@material-ui/core";
+
+const headersData = [
+  {
+    label: "Home",
+    href: "/Home",
+  },
+  {
+    label: "Purchased Books",
+    href: "/PurchasedBooks",
+  },
+  {
+    label: "Log Out",
+    href: "/logout",
+  },
+];
+
+const Header = () => {
+  const {
+    header,
+    logoStyle,
+    menuButton,
+    search,
+    searchIcon,
+    inputRoot,
+    inputInput,
+    menuIcon,
+  } = useStyles();
+
+  const [state, setState] = useState({
+    openMenu: false,
+    anchorEl: null,
+  });
+  const { openMenu, anchorEl } = state;
+  const logo = (
+    <Typography variant="h6" component="h1" className={logoStyle}>
+      Book Store
+    </Typography>
+  );
+
+  const setMobileView = (event) => {
+    let target = event.currentTarget;
+    setState((prevState) => ({ ...prevState, openMenu: true }));
+    setState((prevState) => ({ ...prevState, anchorEl: target }));
+  };
+
+  const handleClose = () => {
+    setState((prevState) => ({ ...prevState, openMenu: false }));
+    setState((prevState) => ({ ...prevState, anchorEl: null }));
+  };
+  const MenuBar = () => {
+    return (
+      <Toolbar>
+        <IconButton onClick={setMobileView}>
+          <div className={menuIcon}>
+            <HomeIcon />
           </div>
-        </Toolbar>
+        </IconButton>
+        {logo}
+        {openMenu ? <MobileDisplay /> : MenuDesktop()}
+        <div className={search}>
+          <div className={searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            className={`${inputRoot} ${inputInput}`}
+            inputProps={{ "aria-label": "search" }}
+          />
+        </div>
+      </Toolbar>
+    );
+  };
+  const MenuDesktop = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Button
+          {...{
+            key: label,
+            color: "inherit",
+            to: href,
+            component: RouterLink,
+            className: menuButton,
+          }}
+          activeStyle={{
+            fontWeight: "bold",
+            borderBottom: "solid 3px #20B2AA	",
+          }}
+        >
+          {label}
+        </Button>
       );
-    };
-    const MenuDesktop = () => {
-      return headersData.map(({ label, href }) => {
-        return (
+    });
+  };
+
+  const MobileDisplay = () => {
+    const data = headersData.map(({ label, href }) => {
+      return (
+        <MenuItem key={label}>
           <Button
             {...{
               key: label,
               color: "inherit",
               to: href,
               component: RouterLink,
-              className: menuButton,
             }}
             activeStyle={{
               fontWeight: "bold",
@@ -101,56 +125,33 @@ import {
           >
             {label}
           </Button>
-        );
-      });
-    };
-  
-    const MobileDisplay = () => {
-      const data = headersData.map(({ label, href }) => {
-        return (
-          <MenuItem key={label}>
-            <Button
-              {...{
-                key: label,
-                color: "inherit",
-                to: href,
-                component: RouterLink,
-              }}
-              activeStyle={{
-                fontWeight: "bold",
-                borderBottom: "solid 3px #20B2AA	",
-              }}
-            >
-              {label}
-            </Button>
-          </MenuItem>
-        );
-      });
-      return (
-        <Menu
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          {data}
-        </Menu>
+        </MenuItem>
       );
-    };
+    });
     return (
-      <header>
-        <AppBar className={header}>
-          <MenuBar />
-        </AppBar>
-      </header>
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {data}
+      </Menu>
     );
   };
-  export default Header;
-  
+  return (
+    <header>
+      <AppBar className={header}>
+        <MenuBar />
+      </AppBar>
+    </header>
+  );
+};
+export default Header;
