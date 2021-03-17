5307a2c6ecdd93a44875a898d46f2b7c6d984968
NuraQ <nura.qasrawi@gmail.com>
2021-03-17 09:27:18 +0200

    redux structure fixed

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,12 +1,20 @@
-import {BookList} from '../../components/BookList/BookList'
-import {HomeLogo} from '../../components/HomeComponents/HomeLogo'
+import { BookList } from "../../components/BookList/BookList";
+import { HomeLogo } from "../../components/HomeComponents/HomeLogo";
+import { useDispatch, useSelector } from "react-redux";
 
-const Home = () =>{
-    return (
-        <div><HomeLogo />
-            <BookList />
-        </div>
-    )
-}
+const Home = () => {
+  const getPage = (state) => state.searchState.page;
+  const currentPage = useSelector(getPage);
+  const dispatch = useDispatch();
+  const selectBooks = (state) => state.searchState.status;
+  const bookQuery = useSelector(selectBooks);
+
+  return (
+    <div>
+      <HomeLogo />
+      <BookList  dispatch={dispatch} currentPage={currentPage} bookQuery={bookQuery} />
+    </div>
+  );
+};
 
 export default Home;
