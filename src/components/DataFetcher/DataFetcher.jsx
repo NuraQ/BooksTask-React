4f3b019a50b531fa3808964e8b1ef3a6aeaf5adc
NuraQ <nura.qasrawi@gmail.com>
2021-03-21 14:33:11 +0200

    switched to axios

--- src/components/DataFetcher/DataFetcher.jsx
+++ src/components/DataFetcher/DataFetcher.jsx
@@ -1,38 +1,39 @@
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 const FetchData = (page, query) => {
   const [Books, setItems] = useState([]);
   const [totalCount, setCount] = useState(0);
   const [hasMore, setHasMore] = useState(false);
   const [loading, setLoading] = useState(true);
-
-
+  const maxResult = 40;
   useEffect(() => {
     setLoading(true)
-    fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=intitle:&maxResults=40&startIndex=${page}&key=AIzaSyCOBbymaad4eBVNFVF5JC-Pc0TQzE6AHOw`,
-      {
-        method: "get",
+    axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=intitle:&maxResults=${maxResult}&startIndex=${page}&key=AIzaSyCOBbymaad4eBVNFVF5JC-Pc0TQzE6AHOw`
+      , {
         headers: {
-          "Content-Type": "application/json",
-        },
-      }
+            'Content-Type': 'application/json'
+        }
+    }
+   
     )
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.totalItems - 40 === page  || data.totalItems  < 0 || data.totalItems  <=  page) {
+      .then((response) => {
+        setHasMore(true);
+        if (response.data.totalItems - 40 === page  || response.data.totalItems  <=  page) {
           setHasMore(false);
           return ;
-        } else {
-          setHasMore(true);
-        }
-        setItems((state) => [...state, ...data.items]);
-        setCount(data.totalItems);
+        } 
+        setItems((state) => [...state, ...response.data.items]);
+        setCount(response.data.totalItems);
         setLoading(false)
       })
       .catch((error) => {
         console.error("Error:", error);
-      });
+      })
+      .finally(()=>{
+        setLoading(false)
+      })
   }, [page]);
   return { Books, totalCount, hasMore, loading };
 };
