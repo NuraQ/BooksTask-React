87b126aedd5f84092e985139b23b488030cf6874
NuraQ <nura.qasrawi@gmail.com>
2021-03-25 10:56:38 +0200

    merged master here

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import {Grid} from '@material-ui/core';
-import {Book} from '../Book/book'
-import { useGridStyles } from './BookList.style';
+import { Grid } from "@material-ui/core";
+import { Book } from "../Book/book";
+import { useGridStyles } from "./BookList.style";
 
- export const BookList = (props) => {
+export const BookList = (props) => {
   const classes = useGridStyles();
   let books = props.books;
   return (
     <div container className={classes.root}>
       <Grid container spacing={3}>
         {books.map((item) => {
-            return (
-              <Book
-                title={item.volumeInfo.title}
-                element={item}
-                key={`${item.id}`}
-                />
-            )
+          return (
+            <Book
+              title={item.volumeInfo.title}
+              element={item}
+              key={`${item.id}`}
+            />
+          );
         })}
       </Grid>
     </div>
